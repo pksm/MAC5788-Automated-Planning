f3e96df683c623815577cd2198b748f4da117661
pksm <paulamoraes95@gmail.com>
2017-11-15 10:22:34 -0200

    Update test1.sh

--- pystrips/test1.sh
+++ pystrips/test1.sh
@@ -1,49 +1,121 @@
 echo
-echo '>> Solving pddl/robot/boxes/problem01.pddl ...'
+echo 'Solving pddl/robot/boxes/problem01.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem01.pddl --heuristics $1 --weight $2
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem01.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem02.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem02.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem03.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem03.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem04.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem04.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem05.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem05.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem06.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem06.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem07.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem07.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem08.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem08.pddl --search $1 
 
 echo
-echo '>> Solving pddl/robot/boxes/problem02.pddl ...'
+echo 'Solving pddl/robot/boxes/problem09.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem02.pddl --heuristics $1 --weight $2
-
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem09.pddl --search $1 
 echo
-echo '>> Solving pddl/robot/boxes/problem03.pddl ...'
+echo 'Solving pddl/robot/boxes/problem10.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem03.pddl --heuristics $1 --weight $2
-
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem10.pddl --search $1 
 echo
-echo '>> Solving pddl/robot/boxes/problem04.pddl ...'
+echo 'Solving pddl/robot/boxes/problem11.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem04.pddl --heuristics $1 --weight $2
-
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem11.pddl --search $1 
 echo
-echo '>> Solving pddl/robot/boxes/problem05.pddl ...'
+echo 'Solving pddl/robot/boxes/problem12.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem05.pddl --heuristics $1 --weight $2
-
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem12.pddl --search $1 
 echo
-echo '>> Solving pddl/robot/boxes/problem06.pddl ...'
+echo 'Solving pddl/robot/boxes/problem13.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem06.pddl --heuristics $1 --weight $2
-
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem13.pddl --search $1 
 echo
-echo '>> Solving pddl/robot/boxes/problem07.pddl ...'
+echo 'Solving pddl/robot/boxes/problem14.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem07.pddl --heuristics $1 --weight $2
-
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem14.pddl --search $1 
 echo
-echo '>> Solving pddl/robot/boxes/problem08.pddl ...'
+echo 'Solving pddl/robot/boxes/problem15.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem08.pddl --heuristics $1 --weight $2
-
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem15.pddl --search $1 
 echo
-echo '>> Solving pddl/robot/boxes/problem09.pddl ...'
+echo 'Solving pddl/robot/boxes/problem16.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem09.pddl --heuristics $1 --weight $2
-
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem16.pddl --search $1 
 echo
-echo '>> Solving pddl/robot/boxes/problem10.pddl ...'
+echo 'Solving pddl/robot/boxes/problem17.pddl ...'
 echo
-python3 pystrips.py solve pddl/robot/domain.pddl pddl/robot/boxes/problem10.pddl --heuristics $1 --weight $2
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem17.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem18.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem18.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem19.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem19.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem20.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem20.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem21.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem21.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem22.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem22.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem23.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem23.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem24.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem24.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem25.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem25.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem26.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem26.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem27.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem27.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem28.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem28.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem29.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem29.pddl --search $1 
+echo
+echo 'Solving pddl/robot/boxes/problem30.pddl ...'
+echo
+python3 main.py pddl/robot/domain.pddl pddl/robot/boxes/problem30.pddl --search $1 
